21fc492ce25ccd6fb718b1fdab13686f223423c6
songyz2019 <songyz2023dlut@outlook.com>
2024-12-03 18:15:46 +0800

    add root_dir

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ from dataloader import FusarShip
 #     T.ToTensor(),
 #     T.Normalize((0.5070751592371323, 0.48654887331495095, 0.4409178433670343), (0.2673342858792401, 0.2564384629170883, 0.27615047132568404))
 # ]))
-testset = FusarShip()
+testset = FusarShip(root_dir='data/fusar_ship')
 
 # Model and Optimizer
 model = MobileShuffle(num_classes = len(testset.CLASS_LIST), inference_mode = False, width_multipliers= [2.0, 2.0, 2.0, 2.0],num_conv_branches= 4, num_blocks_per_stage= [2, 4, 6, 1],use_se= True).cuda()
